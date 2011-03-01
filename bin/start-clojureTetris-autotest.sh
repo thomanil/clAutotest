ab0e1934ba09fb58bcf05d2addf8c4cfb51254f0
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-03-01 22:14:49 +0100

    Simplified and generalized autotest startscript

--- bin/start-clojureTetris-autotest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Autorun tests in my clojureTetris project
-
-java -cp lib/clojure-1.1.0.jar:lib/clojure-contrib-1.1.0.jar:.:classes clojure.main src/clAutotest/core.clj 'lein test' 'clj' 'Exception in%ERROR in%FAIL in' '/Users/thomas/versioncontrolled/github/clojureTetris'
